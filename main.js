a02259830b14b5e3bccca959a051f5b541ce493e
GeorgePanov <georgepanov19@gmail.com>
2023-05-10 22:45:31 +0300

    add js

---
+++ main.js
@@ -0,0 +1,7 @@
+let text = "Эта страница выложена на GitHub и находится в открытом доступе \
+с помощью GitHub Pages. К сожалению, данный проект не поддерживает Backend, \
+поэтому подключить к этой странице созданную модель невозможно";
+
+document.getElementById('button').onclick = function () {
+    alert(text);
+}
