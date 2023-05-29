f2114626333d639a7f17b2108f99714c7ff4a871
GeorgePanov <georgepanov19@gmail.com>
2023-05-29 23:18:54 +0300

    3 div

--- colorChanger/main.js
+++ colorChanger/main.js
@@ -22,7 +22,14 @@ function setRandomColors() {
 
         text.textContent = color;
         col.style.background = color;
+
+        setTextColor(text, color)
     })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
 setRandomColors();
