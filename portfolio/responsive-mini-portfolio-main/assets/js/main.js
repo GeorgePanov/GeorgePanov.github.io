ef2b89f51112c7fe9adfbaf163cae3bd7d369267
GeorgePanov <georgepanov19@gmail.com>
2023-07-13 15:28:58 +0300

    SKILLS and FILTERS TABS

--- portfolio/responsive-mini-portfolio-main/assets/js/main.js
+++ portfolio/responsive-mini-portfolio-main/assets/js/main.js
@@ -1,4 +1,27 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]');
+
+console.log(tabs, tabContent);
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContent.forEach(tc => {
+            tc.classList.remove('filters__active');
+        })
+
+        target.classList.add('filters__active');
+
+
+        tabs.forEach(t => {
+            t.classList.remove('filters-tab-active');
+        })
+
+        tab.classList.add('filters-tab-active');
+    })
+})
 
 
 /*=============== DARK LIGHT THEME ===============*/
